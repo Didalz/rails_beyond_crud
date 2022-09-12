66640c86e4c17c20389c65362dbf88cedcc6d7bf
diego lopez <diegoalejandrolopezzapata@gmail.com>
2022-09-12 18:23:51 -0500

    fix chef error

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :set_restaurant, only: %i[ show edit update destroy chef]
 
   # GET /restaurants
   def index
